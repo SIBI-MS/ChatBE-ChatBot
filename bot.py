b6a0d5d51c1159b32bc0d12d22f6c4b0c69bdd01
SIBI M. S <78226316+SIBI-MS@users.noreply.github.com>
2024-06-07 21:17:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 from langsmith import traceable #for use langsmith to trace with working of code
 from langsmith.schemas import Run, Example 
-from langsmith.evaluation import evaluate
+from langsmith.evaluation import evaluate 
 from langchain_groq import ChatGroq  
  
  
